99cbb01fbc0d5ce4acf45fefd2b0f7d797bcd847
vishnugupthaa <60147011+vishnugupthaa@users.noreply.github.com>
2020-07-08 16:43:54 +0530

    Add files via upload

---
+++ task 4/src/main/java/EPAM/Task_4/MainHouseClass.java
@@ -0,0 +1,14 @@
+package EPAM.Task_4;
+
+import java.io.IOException;
+
+public class MainHouseClass 
+	{
+		    public static void main(String args[]) throws IOException 
+		    {
+		    	HouseConstruction c = new HouseConstruction();
+		    	System.out.println("Cost of Construction of House");
+		    	System.out.println("-----------------------------");
+		    	c.costEstimation();
+		    }
+	}
